e0b597a1fc45e6e3b4d5f02b990058659103a676
muz1kand <Karpoff-net@ya.ru>
2023-11-02 15:14:16 +0300

    Update default_config.sh

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -68,7 +68,7 @@ function customize_image() {
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
     apt-get update
-    apt-get install google-chrome-stable
+    apt-get install -y google-chrome-stable
 
 
 
